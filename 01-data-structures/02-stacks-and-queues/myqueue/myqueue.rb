9c40cb331fcf4dab6b7521df07746b47d1017b33
Nick Brierley <nickcbrierley@gmail.com>
2017-08-08 16:57:36 +0000

    checkpoint2

--- 01-data-structures/02-stacks-and-queues/myqueue/myqueue.rb
+++ 01-data-structures/02-stacks-and-queues/myqueue/myqueue.rb
@@ -5,14 +5,30 @@ class MyQueue
   def initialize
     @queue = Array.new
     @head = @queue[0]
+    @index = 0
   end
 
   def enqueue(element)
+    @queue[@index] = element
+    @head = @queue[0]
+    @tail = @queue[@index]
+    @index += 1
   end
 
   def dequeue
+    temp = @queue[0]
+    @queue.delete_at(0)
+    @head = @queue[0]
+    @index = @queue.length - 1
+    @tail = @queue[@index]
+    return temp
   end
 
   def empty?
+    if @queue.length === 0
+      true
+    else
+      false
+    end
   end
 end
