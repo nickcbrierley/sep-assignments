da6bc516179e8609ad67c4886fdc19fad3467f92
Nick Brierley <nickcbrierley@gmail.com>
2017-08-15 14:50:37 +0000

    checkpoint4

--- 01-data-structures/02-stacks-and-queues/mystack/mystack.rb
+++ 01-data-structures/02-stacks-and-queues/mystack/mystack.rb
@@ -4,14 +4,28 @@ class MyStack
   def initialize
     @stack = Array.new
     self.top = nil
+    @index = 0
   end
 
   def push(item)
+    @index += 1
+    self.top = item
+    @stack[@index] = self.top
   end
 
   def pop
+    item = self.top
+    @stack[@index] = nil
+    @index -= 1
+    self.top = @stack[@index]
+    return item
   end
 
   def empty?
+    if @index > 0 then
+      return false
+    else
+      return true
+    end
   end
 end
--- 01-data-structures/04-hashes-part-1/hashclass_spec.rb
+++ 01-data-structures/04-hashes-part-1/hashclass_spec.rb
@@ -1,3 +1,4 @@
+require 'rspec/autorun'
 include RSpec
 
 require_relative 'hash_item'
