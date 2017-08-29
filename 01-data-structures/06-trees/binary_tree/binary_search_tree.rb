3d681934a91c33ea5a5ceff35718deee5caf674c
Nick Brierley <nickcbrierley@gmail.com>
2017-08-29 18:31:52 +0000

    checkpoint6-missing-files

--- 01-data-structures/06-trees/binary_tree/binary_search_tree.rb
+++ 01-data-structures/06-trees/binary_tree/binary_search_tree.rb
@@ -3,19 +3,38 @@ require_relative 'node'
 class BinarySearchTree
 
   def initialize(root)
+    @root = root
   end
 
   def insert(root, node)
+     if root.rating < node.rating 
+       root.right.nil? ? root.right = node : insert(root.right, node)
+     else
+       root.left.nil? ? root.left = node : insert(root.left, node)
+     end
   end
 
   # Recursive Depth First Search
   def find(root, data)
+    return nil if root.nil? || data.nil?
+    return root if root.title == data
+    root.left ? find(root.left, data) : find(root.right, data)
   end
 
   def delete(root, data)
+    tempNode = find(root, data)
+    tempNode.nil? ? nil : (tempNode.title = nil && tempNode.rating = nil)
   end
 
   # Recursive Breadth First Search
   def printf(children=nil)
+    queue = Queue.new
+    queue.enq(@root)
+    while !queue.empty?
+      value = queue.deq
+      puts "#{value.title}: #{value.rating}"
+      queue.enq(value.left) if value.left
+      queue.enq(value.right) if value.right
+    end
   end
 end
--- 01-data-structures/06-trees/binary_tree/binary_search_tree_spec.rb
+++ 01-data-structures/06-trees/binary_tree/binary_search_tree_spec.rb
@@ -1,3 +1,4 @@
+require 'rspec/autorun'
 include RSpec
 
 require_relative 'binary_search_tree'
