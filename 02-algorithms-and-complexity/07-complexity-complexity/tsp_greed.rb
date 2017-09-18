1488687bd3bb62b83525a15ff718bb4a52835355
Nick Brierley <nickcbrierley@gmail.com>
2017-09-18 21:36:10 +0000

    compcomp

---
+++ 02-algorithms-and-complexity/07-complexity-complexity/tsp_greed.rb
@@ -0,0 +1,13 @@
+def nearest_city(graph_of_cities, current_city)
+    while current_city.visited === false
+        neighbor_cities = current_city.neighbors
+        next_city = neighbor_cities[0]
+        for current_neighbor in neighbor_cities
+            if current_neighbor.distance < next_city.distance
+                current_neighbor = next_city
+            end
+        end
+        current_city.visited = true
+        next_city = current_city
+    end
+end
